644b28246b38536d2951685e9382214c77ab8b5d
EPA Robotics <55808130+epa-robotics-team@users.noreply.github.com>
2024-01-20 11:10:45 -0800

    bring the light show from holiday bot

--- src/main/java/frc/robot/subsystems/LightShow.java
+++ src/main/java/frc/robot/subsystems/LightShow.java
@@ -2,11 +2,11 @@ package frc.robot.subsystems;
 
 import edu.wpi.first.wpilibj.AddressableLED;
 import edu.wpi.first.wpilibj.AddressableLEDBuffer;
-import edu.wpi.first.wpilibj.RobotState;
 import edu.wpi.first.wpilibj.Timer;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 
 public class LightShow extends SubsystemBase {
+
   private final int klightPWM = 9;
 
   // NOTE: you can only allocate ONE strip of LEDs (in series). This is a
@@ -14,93 +14,94 @@ public class LightShow extends SubsystemBase {
   // of how WPILib implements the addressable LED strips.
   // https://www.chiefdelphi.com/t/can-not-allocate-a-second-addressableled-pwm-port/376859
 
-  // Our lights are set up as two 8x32 grids in series.
   AddressableLED leds = new AddressableLED(klightPWM);
   final int ROWS = 8;
   final int COLS = 32;
-  final int STRIPS = 2;
-  final int PIXELS = (ROWS * COLS) * STRIPS;
-  AddressableLEDBuffer pixels = new AddressableLEDBuffer(PIXELS);
-  Timer t = new Timer();
-  double waitTime = 0.0;
-  private final Intake m_Intake;
+  final int PIXELS = (ROWS * COLS) + 196;
+  final int defaultBrightness = 20;
 
-  public LightShow(Intake m_Intake) {
+  AddressableLEDBuffer pixels = new AddressableLEDBuffer(PIXELS);
+
+  String m_lastBuffer = pixels.toString();
+
+  Timer timer = new Timer();
+  double waitTime = 0.0;
+
+  public LightShow() {
     leds.setLength(PIXELS);
     leds.start();
-    t.start();
-    this.m_Intake = m_Intake;
-  }
-
-  public int getPixelCount() {
-    return PIXELS;
-  }
-
-  public void clearPixels() {
-    fill(0, 0, 0);
-  }
-
-  public void fill(int red, int green, int blue) {
-    for (int i = 0; i < PIXELS; i++) {
-      pixels.setRGB(i, red, green, blue);
-    }
-    leds.setData(pixels);
+    timer.start();
   }
 
   public void fillPercentage(int redPercent, int greenPercent, int bluePercent) {
     for (int i = 0; i < PIXELS; i++) {
       pixels.setRGB(i, redPercent, greenPercent, bluePercent);
     }
-    leds.setData(pixels);
   }
 
-  public void setPixels(AddressableLEDBuffer buffer) {
-    boolean isError = buffer.getLength() != PIXELS;
-    if (isError) {
-      fill(255, 0, 0);
-      return;
-    }
-    // if (!priorBuffer.equals(buffer)) {
-    leds.setData(buffer);
-    // }
+  public void setBlue() {
+    fillPercentage(0, 0, defaultBrightness);
   }
 
   public void setPurple() {
-    fillPercentage(5, 0, 5);
-  }
-  public void setBlue() {
-    fillPercentage(0, 0, 5);
+    fillPercentage(defaultBrightness, 0, defaultBrightness);
   }
 
   public void setGreen() {
-    fillPercentage(0, 5, 0);
+    fillPercentage(0, defaultBrightness, 0);
   }
 
   public void setRed() {
-    fillPercentage(5, 0, 0);
+    fillPercentage(defaultBrightness, 0, 0);
+  }
+
+  public void shootTheStar() {
+    var secondsElapsed = timer.get();
+    var lastFrameInSeconds = 5.0;
+    var totalFrames = PIXELS;
+    var currentPercentComplete = secondsElapsed / lastFrameInSeconds;
+    var currentFrame = (int) Math.floor(currentPercentComplete * totalFrames) % (totalFrames - 4);
+    var light1 = currentFrame;
+    var light2 = currentFrame + 1;
+    var light3 = currentFrame + 2;
+    fillPercentage(0, 0, 0);
+    pixels.setRGB(light1, 100, 0, 0);
+    pixels.setRGB(light2, 100, 0, 0);
+    pixels.setRGB(light3, 100, 0, 0);
+  }
+
+  // The lights sort of follow a nonlinear pattern where if x is even
+  // convert x and y coordinates to an index on the light strip
+  int findIndex(int x, int y) {
+    int index;
+    x = 31 - x;// correct from right to left
+    if (x % 2 == 0) {
+      index = (8 * x) + y;
+    } else {
+      index = (8 * x) + (7 - y);
+    }
+    return index;
   }
 
   @Override
   public void periodic() {
-    // runDefaultLights();
-    if (RobotState.isEStopped()) {
-      setRed();
-    } else if (RobotState.isAutonomous()) {
-      setPurple();
-    } else if (m_Intake.isYoinking()) {
-      setGreen();
-    } else if (m_Intake.isYeeting()) {
-      setBlue();
-    } else {
-      turnOff();
+
+    // double currentTime = timer.get();//1.5034234234
+    // double tenTimesFaster = currentTime * 10;//15.034234234
+    // int index = (int) tenTimesFaster;//15
+
+    // int x = 0;
+    // int y = 2;
+    // int index = findIndex(x, y);
+
+    // pixels.setRGB(index % PIXELS, 20, 0, 0);
+    // pixels.setRGB(findIndex(3, 3), 20, 0, 0);
+    var currentBuffer = pixels.toString();
+    var needsUpdate = m_lastBuffer != currentBuffer;
+    if (needsUpdate) {
+      m_lastBuffer = currentBuffer;
+      leds.setData(pixels);
     }
   }
 
-  public void turnOff() {
-    fill(0, 0, 0);
-  }
-
-  public void runDefaultLights() {
-    turnOff();
-  }
 }
